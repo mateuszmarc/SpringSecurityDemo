377a25f2131ce876799b2b7f97e0e6eb98341210
mateuszmarc <mmarckiewicz@gmail.com>
2024-07-01 18:37:18 +0000

    create EmployeeController and home view

---
+++ src/test/java/com/mateuszmarcyk/springmvcsecuritydemo/SpringmvcsecuritydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateuszmarcyk.springmvcsecuritydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmvcsecuritydemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
