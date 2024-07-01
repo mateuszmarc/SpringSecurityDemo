51766c1b8603a7853764a59b47cac9fde4f90b34
mateuszmarc <mmarckiewicz@gmail.com>
2024-07-01 19:07:42 +0000

    Configure Spring Security to reference custom login form.

--- src/main/java/com/mateuszmarcyk/springmvcsecuritydemo/security/EmployeeSecurityConfig.java
+++ src/main/java/com/mateuszmarcyk/springmvcsecuritydemo/security/EmployeeSecurityConfig.java
@@ -2,9 +2,11 @@ package com.mateuszmarcyk.springmvcsecuritydemo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class EmployeeSecurityConfig {
@@ -32,4 +34,36 @@ public class EmployeeSecurityConfig {
 
         return new InMemoryUserDetailsManager(john, mary, susan);
     }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(configurer ->
+                        configurer
+                                .anyRequest().authenticated()
+                )
+                .formLogin(form ->
+                        form
+                                .loginPage("/login")
+                                .loginProcessingUrl("/login")
+                                .permitAll()
+                );
+        return httpSecurity.build();
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
