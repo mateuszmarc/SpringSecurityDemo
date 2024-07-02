82310b08eb2a28476a7f1ed51e49d87bc8dea136
mateuszmarc <mmarckiewicz@gmail.com>
2024-07-02 02:39:31 +0000

    Add dependencies for jpa conneciton. Add logging configuration in properties. Change EmployeeSecurityConfig to read users and roles form database.

--- src/main/java/com/mateuszmarcyk/springmvcsecuritydemo/security/EmployeeSecurityConfig.java
+++ src/main/java/com/mateuszmarcyk/springmvcsecuritydemo/security/EmployeeSecurityConfig.java
@@ -3,36 +3,19 @@ package com.mateuszmarcyk.springmvcsecuritydemo.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class EmployeeSecurityConfig {
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
 
-        UserDetails john = User.builder()
-                .username("john")
-                .password("{noop}test123")
-                .roles("EMPLOYEE")
-                .build();
-
-        UserDetails mary = User.builder()
-                .username("mary")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER")
-                .build();
-
-        UserDetails susan = User.builder()
-                .username("susan")
-                .password("{noop}test123")
-                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(john, mary, susan);
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
@@ -54,6 +37,31 @@ public class EmployeeSecurityConfig {
                         configurer.accessDeniedPage("/access-denied"));
         return httpSecurity.build();
     }
+
+
+    //    @Bean
+//    public InMemoryUserDetailsManager userDetailsManager() {
+
+//        UserDetails john = User.builder()
+//                .username("john")
+//                .password("{noop}test123")
+//                .roles("EMPLOYEE")
+//                .build();
+//
+//        UserDetails mary = User.builder()
+//                .username("mary")
+//                .password("{noop}test123")
+//                .roles("EMPLOYEE", "MANAGER")
+//                .build();
+//
+//        UserDetails susan = User.builder()
+//                .username("susan")
+//                .password("{noop}test123")
+//                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+//                .build();
+
+//        return new InMemoryUserDetailsManager(john, mary, susan);
+//    }
 }
 
 
